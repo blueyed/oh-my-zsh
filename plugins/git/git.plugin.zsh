f5c7d3cb2989dee80869ddabc74869a13fb9877a
Daniel Hahler <git@thequod.de>
2015-11-26 14:45:22 +0100

    Add gbmcleanup: used since a while, and can be used after 'gbm' easily

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -121,6 +121,57 @@ compdef _git gb=git-branch
 
 alias gbm='git branch --merged'
 alias gbnm='git branch --no-merged'
+
+# Delete merged branched.
+gbmcleanup() {
+  zparseopts -a opts f q h i d
+  local curb merged
+  local force interactive
+  local from_branches='^(master|dev)$'
+  local keep_branches='^(master|dev|local)$'
+
+  if (( $opts[(I)-h] )); then
+    echo "Cleans merged branches."
+    echo "$0 [-i] [-f] [-q]"
+    return
+  fi
+  interactive=$opts[(I)-i]
+  force=$opts[(I)-f]
+  dry_run=$opts[(I)-d]
+
+  curb=$(current_branch)
+  if [[ -z $curb ]]; then
+    echo "No current branch. Aborting."
+    return 1
+  fi
+  if ! [[ $curb =~ $from_branches ]] && ! (( $force )) && ! (( $dry_run )); then
+    echo "Current branch does not match '$from_branches'." 2>&1
+    echo "Use -f to force." 2>&1
+    return 1
+  fi
+
+  merged=($(git branch --merged | sed '/^*/d' | cut -b3- | sed "/$keep_branches/d"))
+
+  local cmd
+  cmd=(git branch -d)
+  if (( $dry_run )); then
+    cmd=(echo $cmd)
+  fi
+
+  if (( $interactive )); then
+    echo "$#merged branches to process: " $merged
+
+    for b in $merged; do
+      view +'set ft=diff' =(echo "== $b =="; git show $b)
+      printf "Delete? "
+      read -q || { echo; continue }; echo
+      $cmd $b
+    done
+  else
+    $cmd $merged
+  fi
+}
+
 alias gbl='git blame'
 
 alias gc='git commit -v'
